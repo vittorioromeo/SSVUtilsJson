cdc8739f15045761f431fefb279e3bc671cb74b0
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-04-09 17:11:40 +0200

    Major name refactoring, replaced asContainer

--- include/SSVUtilsJson/Utils/UtilsJson.h
+++ include/SSVUtilsJson/Utils/UtilsJson.h
@@ -9,29 +9,25 @@
 #include <string>
 #include <jsoncpp/json.h>
 #include <jsoncpp/reader.h>
-#include <SSVUtils/SSVUtils.h>
 
 namespace ssvuj
 {
 	template<typename T> T as(const Json::Value& mRoot);
-	template<typename T> T getValue(const Json::Value& mRoot, const std::string& mValue) { return as<T>(mRoot[mValue]); }
-	template<typename T> T getValue(const Json::Value& mArray, unsigned int mIndex) { return as<T>(mArray[mIndex]); }
-	template<typename T> T getValueOrDefault(const Json::Value& mRoot, const std::string& mValue, T mDefault) { return mRoot.isMember(mValue) ? getValue<T>(mRoot, mValue) : mDefault; }
-	template<typename T> T getValueOrDefault(const Json::Value& mArray, unsigned int mIndex, T mDefault) { return mArray.isValidIndex(mIndex) ? getValue<T>(mArray, mIndex) : mDefault; }
+	template<typename T> T as(const Json::Value& mRoot, const std::string& mValue) { return as<T>(mRoot[mValue]); }
+	template<typename T> T as(const Json::Value& mArray, unsigned int mIndex) { return as<T>(mArray[mIndex]); }
+	template<typename T> T asOrDefault(const Json::Value& mRoot, const std::string& mValue, T mDefault) { return mRoot.isMember(mValue) ? as<T>(mRoot, mValue) : mDefault; }
+	template<typename T> T asOrDefault(const Json::Value& mArray, unsigned int mIndex, T mDefault) { return mArray.isValidIndex(mIndex) ? as<T>(mArray, mIndex) : mDefault; }
 
-	template<typename T> T asContainer(const Json::Value& mRoot)
+	template<typename T> std::vector<T> asVector(const Json::Value& mRoot)
 	{
-		typedef typename T::value_type Item;
-		typedef ssvu::Traits::Container<T, Item> ContainerTraits;
-
-		T result;
-		for(unsigned int i{0}; i < mRoot.size(); ++i) ContainerTraits::add(result, getValue<Item>(mRoot, i));
+		std::vector<T> result;
+		for(unsigned int i{0}; i < mRoot.size(); ++i) result.push_back(as<T>(mRoot, i));
 		return result;
 	}
-	template<typename T> T getContainer(const Json::Value& mRoot, const std::string& mValue) { return asContainer<T>(mRoot[mValue]); }
-	template<typename T> T getContainer(const Json::Value& mArray, unsigned int mIndex) { return asContainer<T>(mArray[mIndex]); }
-	template<typename T> T getContainerOrDefault(const Json::Value& mRoot, const std::string& mValue, T mDefault) { return mRoot.isMember(mValue) ? getContainer<T>(mRoot, mValue) : mDefault; }
-	template<typename T> T getContainerOrDefault(const Json::Value& mArray, unsigned int mIndex, T mDefault) { return mArray.isValidIndex(mIndex) ? getContainer<T>(mArray, mIndex) : mDefault; }
+	template<typename T> std::vector<T> asVector(const Json::Value& mRoot, const std::string& mValue) { return asVector<T>(mRoot[mValue]); }
+	template<typename T> std::vector<T> asVector(const Json::Value& mArray, unsigned int mIndex) { return asVector<T>(mArray[mIndex]); }
+	template<typename T> std::vector<T> asVectorOrDefault(const Json::Value& mRoot, const std::string& mValue, std::vector<T> mDefault) { return mRoot.isMember(mValue) ? asVector<T>(mRoot, mValue) : mDefault; }
+	template<typename T> std::vector<T> asVectorOrDefault(const Json::Value& mArray, unsigned int mIndex, std::vector<T> mDefault) { return mArray.isValidIndex(mIndex) ? asVector<T>(mArray, mIndex) : mDefault; }
 
 	Json::Value getRootFromString(const std::string& mString);
 	Json::Value getRootFromFile(const std::string& mPath);
