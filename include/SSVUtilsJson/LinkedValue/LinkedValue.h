745faebdf7cdade99ccc955c45d830bacd54d673
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-23 11:02:02 +0200

    Added LinkedValueManager, which manages a collection of LinkedValues

--- include/SSVUtilsJson/LinkedValue/LinkedValue.h
+++ include/SSVUtilsJson/LinkedValue/LinkedValue.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <string>
+#include <SSVUtils/SSVUtils.h>
 #include "SSVUtilsJson/Utils/UtilsJson.h"
 
 namespace ssvuj
@@ -18,9 +19,10 @@ namespace ssvuj
 
 		public:
 			LinkedValueBase(const std::string& mLinkedName) : linkedName{mLinkedName} { }
+			virtual ~LinkedValueBase() { }
 
-			virtual void syncFrom(const Json::Value& mRoot) = 0;
-			virtual void syncTo(Json::Value& mRoot) = 0;
+			inline virtual void syncFrom(const Json::Value& mRoot) = 0;
+			inline virtual void syncTo(Json::Value& mRoot) const = 0;
 	};
 
 	template<typename T> class LinkedValue : public LinkedValueBase
@@ -30,12 +32,30 @@ namespace ssvuj
 
 		public:
 			LinkedValue(const std::string& mLinkedName) : LinkedValueBase{mLinkedName} { }
+			~LinkedValue() { }
 
 			inline operator T() { return value; }
 			inline LinkedValue& operator=(T mValue) { value = mValue; return *this; }
 
-			void syncFrom(const Json::Value& mRoot) override { value = ssvuj::as<T>(mRoot, linkedName); }
-			void syncTo(Json::Value& mRoot) override { mRoot[linkedName] = value; }
+			inline void syncFrom(const Json::Value& mRoot) override { value = ssvuj::as<T>(mRoot, linkedName); }
+			inline void syncTo(Json::Value& mRoot) const override { mRoot[linkedName] = value; }
+	};
+
+	class LinkedValueManager
+	{
+		private:
+			Json::Value& linkedRoot;
+			ssvu::MemoryManager<LinkedValueBase> memoryManager;
+
+		public:
+			LinkedValueManager(Json::Value& mLinkedRoot) : linkedRoot{mLinkedRoot} { }
+
+			template<typename T> LinkedValue<T>& create(const std::string& mLinkedName) { return memoryManager.create<LinkedValue<T>>(mLinkedName); }
+
+			inline void syncFromRoot() { for(auto& lv : memoryManager) lv->syncFrom(linkedRoot); }
+			inline void syncToRoot() { for(const auto& lv : memoryManager) lv->syncTo(linkedRoot); }
+
+			inline const std::vector<LinkedValueBase*>& getLinkedValues() { return memoryManager.getItems(); }
 	};
 }
 
