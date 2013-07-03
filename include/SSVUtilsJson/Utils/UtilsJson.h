2b8164f3cd895379798a3800bed5a64202c03c7d
Vittorio Romeo <vittorioromeo@veeDesktop-arch.homenet.telecomitalia.it>
2013-07-03 15:46:50 +0200

    Added more utility methods (preparing migration to rapidJson?)

--- include/SSVUtilsJson/Utils/UtilsJson.h
+++ include/SSVUtilsJson/Utils/UtilsJson.h
@@ -12,6 +12,13 @@
 
 namespace ssvuj
 {
+	unsigned int size(const Json::Value& mArray) { return mArray.size(); }
+	unsigned int size(const Json::Value& mRoot, const std::string& mValue) { return mRoot[mValue].size(); }
+
+	bool has(const Json::Value& mRoot, const std::string& mValue) { return mRoot.isMember(mValue); }
+
+	template<typename T> void set(Json::Value& mRoot, const std::string& mValue, T mValueToSet) { mRoot[mValue] = mValueToSet; }
+
 	template<typename T> T as(const Json::Value& mRoot) { return Internal::AsHelper<T>::as(mRoot); }
 	template<typename T> T as(const Json::Value& mRoot, const std::string& mValue) { return as<T>(mRoot[mValue]); }
 	template<typename T> T as(const Json::Value& mArray, unsigned int mIndex) { return as<T>(mArray[mIndex]); }
