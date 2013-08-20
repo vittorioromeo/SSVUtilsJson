e6fbc11985d111773bee3a97b74a7b34eae3a45b
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-20 12:42:08 +0200

    Unified map and unordered_map, shortened code by using inheritance for default toObj

--- include/SSVUtilsJson/Utils/DefaultConverters.h
+++ include/SSVUtilsJson/Utils/DefaultConverters.h
@@ -21,67 +21,25 @@ namespace ssvuj
 		template<typename T> struct Converter
 		{
 			inline static void fromObj(T& mValue, const Obj& mObj);
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
+			inline static void toObj(Obj& mObj, const T& mValue) { mObj = mValue; }
 		};
-		template<> struct Converter<Obj>
-		{
-			using T = Obj;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj; }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<char>
-		{
-			using T = char;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = static_cast<T>(mObj.asInt()); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<unsigned char>
-		{
-			using T = unsigned char;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = static_cast<T>(mObj.asInt()); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<int>
-		{
-			using T = int;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asInt(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
+		template<typename T> struct CDefaultToObj { inline static void toObj(Obj& mObj, const T& mValue) { mObj = mValue; } };
+
+		template<> struct Converter<Obj> : CDefaultToObj<Obj>						{ using T = Obj;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj; } };
+		template<> struct Converter<char> : CDefaultToObj<char>						{ using T = char;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = static_cast<T>(mObj.asInt()); } };
+		template<> struct Converter<unsigned char> : CDefaultToObj<unsigned char>	{ using T = unsigned char;	inline static void fromObj(T& mValue, const Obj& mObj) { mValue = static_cast<T>(mObj.asInt()); } };
+		template<> struct Converter<int> : CDefaultToObj<int>						{ using T = int;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asInt(); } };
+		template<> struct Converter<float> : CDefaultToObj<float>					{ using T = float;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asFloat(); } };
+		template<> struct Converter<double> : CDefaultToObj<double>					{ using T = double;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asDouble(); } };
+		template<> struct Converter<bool> : CDefaultToObj<bool>						{ using T = bool;			inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asBool(); } };
+		template<> struct Converter<std::string> : CDefaultToObj<std::string>		{ using T = std::string;	inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asString(); } };
+		template<> struct Converter<const char*> : CDefaultToObj<const char*>		{ using T = const char*;	inline static void fromObj(T& mValue, const Obj& mObj) { mValue = mObj.asCString(); } };
+
 		template<> struct Converter<long>
 		{
 			using T = long;
 			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asLargestInt(); }
 			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = static_cast<Json::Value::Int64>(mValue); }
-		};
-		template<> struct Converter<float>
-		{
-			using T = float;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asFloat(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<double>
-		{
-			using T = double;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asDouble(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<bool>
-		{
-			using T = bool;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asBool(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<std::string>
-		{
-			using T = std::string;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asString(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
-		};
-		template<> struct Converter<const char*>
-		{
-			using T = const char*;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ mValue = mObj.asCString(); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ mObj = mValue; }
 		};
 		template<> struct Converter<unsigned int>
 		{
@@ -101,15 +59,9 @@ namespace ssvuj
 			inline static void fromObj(T& mValue, const Obj& mObj)	{ for(auto i(0u); i < mObj.size(); ++i) mValue.push_back(getFromObj<TItem>(mObj[i])); }
 			inline static void toObj(Obj& mObj, const T& mValue)	{ for(auto i(0u); i < mValue.size(); ++i) mObj[i] = getToObj<TItem>(mValue[i]); }
 		};
-		template<typename TKey, typename TValue> struct Converter<std::map<TKey, TValue>>
+		template<typename TKey, typename TValue, template<typename, typename, typename...> class TMap> struct Converter<TMap<TKey, TValue>>
 		{
-			using T = std::map<TKey, TValue>;
-			inline static void fromObj(T& mValue, const Obj& mObj)	{ for(auto itr(mObj.begin()); itr != mObj.end(); ++itr) mValue[itr.key().asString()] = getFromObj<TValue>(*itr); }
-			inline static void toObj(Obj& mObj, const T& mValue)	{ for(const auto& p : mValue) mObj[ssvu::toStr(p.first)] = getToObj<TValue>(p.second); }
-		};
-		template<typename TKey, typename TValue> struct Converter<std::unordered_map<TKey, TValue>>
-		{
-			using T = std::unordered_map<TKey, TValue>;
+			using T = TMap<TKey, TValue>;
 			inline static void fromObj(T& mValue, const Obj& mObj)	{ for(auto itr(mObj.begin()); itr != mObj.end(); ++itr) mValue[itr.key().asString()] = getFromObj<TValue>(*itr); }
 			inline static void toObj(Obj& mObj, const T& mValue)	{ for(const auto& p : mValue) mObj[ssvu::toStr(p.first)] = getToObj<TValue>(p.second); }
 		};
