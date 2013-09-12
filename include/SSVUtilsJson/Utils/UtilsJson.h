149bb2f2d95d731d3be59b3060be17b08a2fc1f9
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-09-12 16:30:34 +0200

    Name refactor

--- include/SSVUtilsJson/Utils/UtilsJson.h
+++ include/SSVUtilsJson/Utils/UtilsJson.h
@@ -11,26 +11,26 @@
 namespace ssvuj
 {
 	inline static Obj& get(Obj& mObj, const Key& mKey)				{ return mObj[mKey]; }
-	inline static Obj& get(Obj& mArray, Idx mIndex)					{ return mArray[mIndex]; }
+	inline static Obj& get(Obj& mArray, Idx mIdx)					{ return mArray[mIdx]; }
 	inline static const Obj& get(const Obj& mObj, const Key& mKey)	{ return mObj[mKey]; }
-	inline static const Obj& get(const Obj& mArray, Idx mIndex)		{ return mArray[mIndex]; }
+	inline static const Obj& get(const Obj& mArray, Idx mIdx)		{ return mArray[mIdx]; }
 
 	inline static unsigned int size(const Obj& mArray)					{ return mArray.size(); }
 	inline static unsigned int size(const Obj& mObj, const Key& mKey)	{ return get(mObj, mKey).size(); }
 
 	inline static bool has(const Obj& mObj, const Key& mKey)	{ return mObj.isMember(mKey); }
-	inline static bool has(const Obj& mArray, Idx mIndex)		{ return size(mArray) > mIndex; }
+	inline static bool has(const Obj& mArray, Idx mIdx)			{ return size(mArray) > mIdx; }
 
 	template<typename T> inline static void set(Obj& mObj, const T& mValue)						{ Internal::Converter<T>::toObj(mObj, mValue); }
 	template<typename T> inline static void set(Obj& mObj, const Key& mKey, const T& mValue)	{ set(get(mObj, mKey), mValue); }
-	template<typename T> inline static void set(Obj& mArray, Idx mIndex, const T& mValue)		{ set(get(mArray, mIndex), mValue); }
+	template<typename T> inline static void set(Obj& mArray, Idx mIdx, const T& mValue)			{ set(get(mArray, mIdx), mValue); }
 
 
 	template<typename T> inline static T as(const Obj& mObj)										{ return Internal::getFromObj<T>(mObj); }
 	template<typename T> inline static T as(const Obj& mObj, const Key& mKey)						{ return as<T>(get(mObj, mKey)); }
-	template<typename T> inline static T as(const Obj& mArray, Idx mIndex)							{ return as<T>(get(mArray, mIndex)); }
+	template<typename T> inline static T as(const Obj& mArray, Idx mIdx)							{ return as<T>(get(mArray, mIdx)); }
 	template<typename T> inline static T as(const Obj& mObj, const Key& mKey, const T& mDefault)	{ return has(mObj, mKey) ? as<T>(mObj, mKey) : mDefault; }
-	template<typename T> inline static T as(const Obj& mArray, Idx mIndex, const T& mDefault)		{ return has(mArray, mIndex) ? as<T>(mArray, mIndex) : mDefault; }
+	template<typename T> inline static T as(const Obj& mArray, Idx mIdx, const T& mDefault)			{ return has(mArray, mIdx) ? as<T>(mArray, mIdx) : mDefault; }
 
 	inline static Iterator begin(Obj& mObj) noexcept			{ return mObj.begin(); }
 	inline static Iterator end(Obj& mObj) noexcept				{ return mObj.end(); }
