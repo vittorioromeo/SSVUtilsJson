0e55a8a48b24ef7d9c19896f24a38ccec08731c3
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-09-17 10:59:28 +0200

    JsonCpp is now part of SSVUJ

--- include/SSVUtilsJson/SSVUtilsJson.h
+++ include/SSVUtilsJson/SSVUtilsJson.h
@@ -5,6 +5,8 @@
 #ifndef SSVUTILSJSON
 #define SSVUTILSJSON
 
+#include "SSVUtilsJson/JsonCpp/json.h"
+#include "SSVUtilsJson/JsonCpp/jsoncpp.inl"
 #include "SSVUtilsJson/Utils/UtilsJson.h"
 #include "SSVUtilsJson/Utils/DefaultConverters.h"
 #include "SSVUtilsJson/Utils/Serialization.h"
