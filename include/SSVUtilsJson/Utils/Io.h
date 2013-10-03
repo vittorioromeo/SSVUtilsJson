cdb1028a4277a753e6f9560c791d0b03e2c3e634
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-10-03 11:53:39 +0200

    Adapted to newest libs Name refactoring

--- include/SSVUtilsJson/Utils/Io.h
+++ include/SSVUtilsJson/Utils/Io.h
@@ -25,11 +25,11 @@ namespace ssvuj
 		}
 	}
 
-	inline static Obj readFromString(const std::string& mString)	{ Obj result; Reader reader; Internal::tryParse(result, reader, mString); return result; }
-	inline static Obj readFromFile(const Path& mPath)				{ Obj result; Reader reader; Internal::tryParse(result, reader, ssvu::FileSystem::getFileContents(mPath)); return result; }
+	inline static Obj readFromString(const std::string& mStr)	{ Obj result; Reader reader; Internal::tryParse(result, reader, mStr); return result; }
+	inline static Obj readFromFile(const Path& mPath)			{ Obj result; Reader reader; Internal::tryParse(result, reader, ssvu::FileSystem::getFileContents(mPath)); return result; }
 
 	template<typename T> inline static void writeToStream(const Obj& mObj, T& mStream)	{ Writer writer; writer.write(mStream, mObj); mStream.flush(); }
-	inline static void writeToString(const Obj& mObj, std::string& mString)				{ std::ostringstream o; writeToStream(mObj, o); mString = o.str(); }
+	inline static void writeToString(const Obj& mObj, std::string& mStr)				{ std::ostringstream o; writeToStream(mObj, o); mStr = o.str(); }
 	inline static void writeToFile(const Obj& mObj, const Path& mPath)					{ std::ofstream o{mPath}; writeToStream(mObj, o); o.close(); }
 	inline static std::string getWriteToString(const Obj& mObj)							{ std::string result; writeToString(mObj, result); return result; }
 }
